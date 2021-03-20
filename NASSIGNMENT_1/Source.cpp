42fa54f0de06d164b997cd6fece43125eb1554e1
Sajeel Ahmad <52258972+sajeell@users.noreply.github.com>
2021-03-20 20:44:31 +0500

    :tada: Complete Task 4

--- NASSIGNMENT_1/Source.cpp
+++ NASSIGNMENT_1/Source.cpp
@@ -13,6 +13,110 @@ int countDecimals(double num)
 	return subStr.length();
 }
 
+double getAdditionAE(double a, double b)
+{
+	double sum = a + b;
+
+	double errorOfA = 0.5 * pow(10, -countDecimals(a));
+	double errorOfB = 0.5 * pow(10, -countDecimals(b));
+
+	double absoluteError = abs(errorOfA) + abs(errorOfB);
+
+	return absoluteError;
+}
+
+double getAdditionRE(double a, double b)
+{
+	double sum = a + b;
+
+	double errorOfA = 0.5 * pow(10, -countDecimals(a));
+	double errorOfB = 0.5 * pow(10, -countDecimals(b));
+
+	double absoluteError = abs(errorOfA) + abs(errorOfB);
+
+	double relativeError = absoluteError / abs(sum);
+
+	return relativeError;
+}
+
+double getSubtractionAE(double a, double b)
+{
+	double sum = a + b;
+
+	double errorOfA = 0.5 * pow(10, -countDecimals(a));
+	double errorOfB = 0.5 * pow(10, -countDecimals(b));
+
+	double absoluteError = abs(errorOfA) + abs(errorOfB);
+
+	return absoluteError;
+}
+
+double getSubtractionRE(double a, double b)
+{
+	double sum = a + b;
+
+	double errorOfA = 0.5 * pow(10, -countDecimals(a));
+	double errorOfB = 0.5 * pow(10, -countDecimals(b));
+
+	double absoluteError = abs(errorOfA) + abs(errorOfB);
+
+	double relativeError = absoluteError / abs(sum);
+
+	return relativeError;
+}
+
+double getMultiplicationAE(double a, double b)
+{
+	double product = a * b;
+
+	double errorOfA = 0.5 * pow(10, -countDecimals(a));
+	double errorOfB = 0.5 * pow(10, -countDecimals(b));
+
+	double relativeError = abs(errorOfA / a) + abs(errorOfB / b);
+
+	double absoluteError = relativeError * product;
+
+	return absoluteError;
+}
+
+double getMultiplicationRE(double a, double b)
+{
+	double product = a * b;
+
+	double errorOfA = 0.5 * pow(10, -countDecimals(a));
+	double errorOfB = 0.5 * pow(10, -countDecimals(b));
+
+	double relativeError = abs(errorOfA / a) + abs(errorOfB / b);
+
+	return relativeError;
+}
+
+double getDivisionAE(double a, double b)
+{
+	double product = a * b;
+
+	double errorOfA = 0.5 * pow(10, -countDecimals(a));
+	double errorOfB = 0.5 * pow(10, -countDecimals(b));
+
+	double relativeError = abs(errorOfA / a) + abs(errorOfB / b);
+
+	double absoluteError = relativeError * product;
+
+	return absoluteError;
+}
+
+double getDivisionRE(double a, double b)
+{
+	double product = a * b;
+
+	double errorOfA = 0.5 * pow(10, -countDecimals(a));
+	double errorOfB = 0.5 * pow(10, -countDecimals(b));
+
+	double relativeError = abs(errorOfA / a) + abs(errorOfB / b);
+
+	return relativeError;
+}
+
 double getPowerAE(double a, double b)
 {
 	double answer = a / b;
@@ -62,6 +166,70 @@ double getRootRE(double a, double power)
 
 	return relativeError;
 }
+
+
+// The range functions
+
+void getRangeAdd(double a, double b)
+{
+	double absoluteError = getAdditionAE(a, b);
+
+	double answer = a + b;
+
+	cout << "The range: " << endl;
+	cout << "Upper limit: " << answer + absoluteError << endl;
+	cout << "Lower limit: " << answer - absoluteError << endl;
+	cout << endl;
+}
+
+void getRangeSub(double a, double b)
+{
+	double absoluteError = getSubtractionAE(a, b);
+
+	double answer = a - b;
+
+	cout << "The range: " << endl;
+	cout << "Upper limit: " << answer + absoluteError << endl;
+	cout << "Lower limit: " << answer - absoluteError << endl;
+	cout << endl;
+}
+
+void getRangeMul(double a, double b)
+{
+	double absoluteError = getMultiplicationAE(a, b);
+
+	double answer = a * b;
+
+	cout << "The range: " << endl;
+	cout << "Upper limit: " << answer + absoluteError << endl;
+	cout << "Lower limit: " << answer - absoluteError << endl;
+	cout << endl;
+}
+
+void getRangeDiv(double a, double b)
+{
+	double absoluteError = getDivisionAE(a, b);
+
+	double answer = a / b;
+
+	cout << "The range: " << endl;
+	cout << "Upper limit: " << answer + absoluteError << endl;
+	cout << "Lower limit: " << answer - absoluteError << endl;
+	cout << endl;
+}
+
+void getRangePower(double a, double power)
+{
+	double absoluteError = getPowerAE(a, power);
+
+	double answer = pow(a, power);
+
+	cout << "The range: " << endl;
+	cout << "Upper limit: " << answer + absoluteError << endl;
+	cout << "Lower limit: " << answer - absoluteError << endl;
+	cout << endl;
+}
+
 
 int main()
 {
