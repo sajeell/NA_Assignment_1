19f6f6b0f7ed5fa98d073146bb2f844876bc54bd
Sajeel Ahmad <52258972+sajeell@users.noreply.github.com>
2021-03-20 16:47:10 +0500

    :construction: Complete Multiplication Question 2

--- NASSIGNMENT_1/Source.cpp
+++ NASSIGNMENT_1/Source.cpp
@@ -39,7 +39,60 @@ double getAdditionRE(double a, double b)
 	return relativeError;
 }
 
+double getSubtractionAE(double a, double b)
+{
+	double sum = a + b;
+
+	double errorOfA = 0.5 * pow(10, -countDecimals(a));
+	double errorOfB = 0.5 * pow(10, -countDecimals(b));
+
+	double absoluteError = abs(errorOfA) + abs(errorOfB);
+
+	return absoluteError;
+}
+
+double getSubtractionRE(double a, double b)
+{
+	double sum = a + b;
+
+	double errorOfA = 0.5 * pow(10, -countDecimals(a));
+	double errorOfB = 0.5 * pow(10, -countDecimals(b));
+
+	double absoluteError = abs(errorOfA) + abs(errorOfB);
+
+	double relativeError = absoluteError / abs(sum);
+
+	return relativeError;
+}
+
+
+double getMultiplicationAE(double a, double b)
+{
+	double product = a * b;
+
+	double errorOfA = 0.5 * pow(10, -countDecimals(a));
+	double errorOfB = 0.5 * pow(10, -countDecimals(b));
+
+	double relativeError = abs(errorOfA / a) + abs(errorOfB / b);
+
+	double absoluteError = relativeError * product;
+
+	return absoluteError;
+}
+
+double getMultiplicationRE(double a, double b)
+{
+	double product = a * b;
+
+	double errorOfA = 0.5 * pow(10, -countDecimals(a));
+	double errorOfB = 0.5 * pow(10, -countDecimals(b));
+
+	double relativeError = abs(errorOfA / a) + abs(errorOfB / b);
+
+	return relativeError;
+}
+
 int main()
 {
-	cout << "checking";
+	cout << getMultiplicationRE(4.0643, 0.37487);
 }
