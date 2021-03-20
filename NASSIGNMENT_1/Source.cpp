25b135d3d7cdda6f26ec14018a8a18705c7e9d2b
Sajeel Ahmad <52258972+sajeell@users.noreply.github.com>
2021-03-20 16:14:10 +0500

    :tada: Question 1

--- NASSIGNMENT_1/Source.cpp
+++ NASSIGNMENT_1/Source.cpp
@@ -2,44 +2,21 @@
 #include <string>
 using namespace std;
 
-int countDecimals(double num)
-{
-	string strNum = to_string(num);
-	strNum.erase(strNum.find_last_not_of('0') + 1, string::npos);
+int getSignificantNumbers(string x) {
+	int mainCounter = 0, zerosCounter = 0;
+	for (int i = 0, length = x.length(); i < length; i++) {
+		if (x[i] == '0') {
+			zerosCounter++;
+		}
+		else if (x[i] != '0' && x[i] != '.') {
+			mainCounter += zerosCounter + 1;
+			zerosCounter = 0;
+		}
+	}
 
-	int indexOfPoint = strNum.find(".");
-	string subStr = strNum.substr(indexOfPoint + 1);
-
-	return subStr.length();
+	return mainCounter;
 }
-
-double getAdditionAE(double a, double b)
-{
-	double sum = a + b;
-
-	double errorOfA = 0.5 * pow(10, -countDecimals(a));
-	double errorOfB = 0.5 * pow(10, -countDecimals(b));
-
-	double absoluteError = abs(errorOfA) + abs(errorOfB);
-
-	return absoluteError;
-}
-
-double getAdditionRE(double a, double b)
-{
-	double sum = a + b;
-
-	double errorOfA = 0.5 * pow(10, -countDecimals(a));
-	double errorOfB = 0.5 * pow(10, -countDecimals(b));
-
-	double absoluteError = abs(errorOfA) + abs(errorOfB);
-
-	double relativeError = absoluteError / abs(sum);
-
-	return relativeError;
-}
-
 int main()
 {
-
+	cout << getSignificantNumbers("1.200020000");
 }
